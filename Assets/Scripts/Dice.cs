a8ba394c68ad1af69a2391873042449524cfb07e
Martin Zackrisson <martin.zackrisson@fripost.org>
2025-04-05 11:15:29 +0200

    work on dice

---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,227 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public delegate void DiceRollEvent(Dice dice);
+
+public class Dice : MonoBehaviour
+{
+    public static DiceRollEvent OnRoll;
+
+    [SerializeField, Header("Dice properties")]
+    int upValue = 1;
+    [SerializeField]
+    Transform upSentinel;
+
+    [SerializeField]
+    int downValue = 6;
+    [SerializeField]
+    Transform downSentinel;
+
+    [SerializeField]
+    int westValue = 2;
+    [SerializeField]
+    Transform westSentinel;
+
+    [SerializeField]
+    int eastValue = 5;
+    [SerializeField]
+    Transform eastSentinel;
+
+    [SerializeField]
+    int northValue = 3;
+    [SerializeField]
+    Transform northSentinel;
+
+    [SerializeField]
+    int southValue = 4;
+    [SerializeField]
+    Transform southSentinel;
+
+    [SerializeField, Header("Roll")]
+    int bounces = 3;
+
+    [SerializeField]
+    Vector2 arcScale = new Vector2(0.5f, 1f);
+
+    [SerializeField]
+    float arcSizeDecay = 0.5f;
+
+    [SerializeField]
+    float rollDuration = 1.5f;
+
+    [SerializeField]
+    float spinFactor = 100f;
+
+    IEnumerable<Transform> Sentinels
+    {
+        get
+        {
+            yield return upSentinel;
+            yield return downSentinel;
+            yield return westSentinel;
+            yield return eastSentinel;
+            yield return northSentinel;
+            yield return southSentinel;
+        }
+    }
+
+    public int Value
+    {
+        get
+        {
+            var highest = Sentinels.OrderBy(s => s.transform.position.y).Last();
+
+            if (highest == upSentinel) return upValue;
+            if (highest == downSentinel) return downValue;
+            if (highest == westSentinel) return westValue;
+            if (highest == eastSentinel) return eastValue;
+            if (highest == northSentinel) return northValue;
+            return southValue;
+        }
+    }
+
+    [ContextMenu("Roll")]
+    public void Roll() => Roll(Vector3.zero);
+
+    bool rolling;
+    Vector3 rollOrigin;
+    List<float> checkpoints;
+    float[] relativeParts;
+    int lastArc = -1;
+    Vector3 spinVector;
+
+    public void Roll(Vector3 origin)
+    {
+        if (rolling) return;
+
+        rolling = true;
+        rollOrigin = origin;
+
+        float t0 = Time.timeSinceLevelLoad;
+        checkpoints = new List<float>() { t0 } ;
+
+        relativeParts = new float[bounces];
+        relativeParts[0] = 1f;
+
+        float partSize = 1f;
+        float totalParts = 1f;
+        for (int i = 1; i<relativeParts.Length; i++)
+        {
+            partSize *= arcSizeDecay;
+            relativeParts[i] = partSize;
+            totalParts += partSize;
+        }
+
+        float timePerPart = rollDuration / totalParts;
+        float time = t0;
+        for (int i = 0; i<relativeParts.Length; i++)
+        {
+            time += timePerPart * relativeParts[i];
+            checkpoints.Add(time);
+        }
+
+        lastArc = -1;
+    }
+
+    private void Update()
+    {
+        if (!rolling) return;
+
+        var t = Time.timeSinceLevelLoad;
+        int arcIdx = checkpoints.FindIndex(c => c > t) - 1;
+        float arcProgress = 0;
+        bool finalizeRoll = false;
+
+        if (arcIdx < 0)
+        {
+            arcProgress = 1f;
+            arcIdx = relativeParts.Length - 1;
+            finalizeRoll = true;
+        } else
+        {
+
+            arcProgress = Mathf.Clamp01((t - checkpoints[arcIdx]) / (checkpoints[arcIdx + 1] - checkpoints[arcIdx]));
+        }
+
+        if (arcIdx > lastArc)
+        {
+            SetSpinn();
+            lastArc = arcIdx;
+        }
+
+        var e = transform.eulerAngles;
+        e += spinVector * Time.deltaTime * spinFactor;
+        transform.eulerAngles = e;
+
+        // Debug.Log($"{arcIdx}: {arcProgress} / {relativeParts[arcIdx]}");
+
+        float yOffset = relativeParts[arcIdx] * 
+            arcScale.y * 
+            Mathf.Sin(arcIdx == 0 ? Mathf.Lerp(Mathf.PI / 2, 0, arcProgress) : Mathf.Lerp(Mathf.PI, 0, arcProgress));
+
+        float xOffset = 0;
+        for (int i=0; i<arcIdx;i++)
+        {
+            if (i == 0)
+            {
+                xOffset += arcScale.x * relativeParts[arcIdx];
+            } else
+            {
+                xOffset += arcScale.x * 3 * relativeParts[arcIdx];
+            }
+        }
+
+        if (arcIdx == 0)
+        {
+            xOffset += relativeParts[arcIdx] * arcScale.x * 0.5f * Mathf.Cos(Mathf.Lerp(Mathf.PI / 2, 0, arcProgress));
+        } else
+        {
+            xOffset += relativeParts[arcIdx] * arcScale.x * 0.5f * (1f + Mathf.Cos(Mathf.Lerp(Mathf.PI, 0, arcProgress)));
+        }
+
+        // Debug.Log(xOffset);
+
+
+        transform.position = rollOrigin + new Vector3(xOffset, yOffset);
+
+        if (finalizeRoll) FinalizeRoll();
+    }
+
+    void SetSpinn()
+    {
+        spinVector = new Vector3(Random.Range(-90, 90), Random.Range(-90, 90), Random.Range(-90, 90));
+        float v = Random.value;
+        if (v < 0.45f)
+        {
+            spinVector.x = 0f;
+        }
+        else if (v < 0.66f)
+        {
+            spinVector.y = 0f;
+        }
+        else
+        {
+            spinVector.z = 0f;
+        }
+        spinVector.Normalize();
+    }
+
+    void FinalizeRoll()
+    {
+        rolling = false;
+        var euler = transform.eulerAngles;
+        euler.x = Mathf.Round(euler.x / 90f) * 90f;
+        euler.y = Mathf.Round(euler.y / 90f) * 90f;
+        euler.z = Mathf.Round(euler.z / 90f) * 90f;
+        transform.eulerAngles = euler;
+
+        OnRoll?.Invoke(this);
+    }
+
+    [ContextMenu("Info")]
+    void Info()
+    {
+        Debug.Log($"{name}: {Value}");
+    }
+}
---
+++ Assets/Scripts/DiceHand.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceHand : MonoBehaviour
+{
+    [SerializeField]
+    Dice startDice;
+
+    [SerializeField]
+    int startDiceCount = 6;
+
+    [SerializeField]
+    Vector3 diceSpacing = new Vector3(-0.25f, 0f, 0f);
+
+    [SerializeField]
+    float timeBetweenDice = 0.5f;
+
+    List<Dice> dice = new List<Dice>();
+
+    private void Start()
+    {
+
+        for (int i = 0; i<startDiceCount; i++)
+        {
+            var die = Instantiate(startDice, transform);
+            die.gameObject.SetActive(false);
+            dice.Add(die);
+        }
+        
+    }
+
+    [ContextMenu("Roll Hand")]
+    void RollHand()
+    {
+        rolling = true;
+        nextRoll = Time.timeSinceLevelLoad;
+    }
+
+    bool rolling;
+    int rollIdx = 0;
+    float nextRoll;
+
+    private void Update()
+    {
+        if (!rolling || Time.timeSinceLevelLoad < nextRoll) return;
+
+        nextRoll = Time.timeSinceLevelLoad + timeBetweenDice;
+        var die = dice[rollIdx];
+        die.gameObject.SetActive(true);
+        var start = transform.TransformVector(diceSpacing * rollIdx);
+        die.transform.position = start;
+        die.Roll(start);
+        rollIdx++;
+
+        if (rollIdx >= dice.Count)
+        {
+            rolling = false;
+        }
+    }
+}
